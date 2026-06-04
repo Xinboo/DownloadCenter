18b21b7ea776c3e23b126bfb6745bb7818f6f3b9
Xinboo <anduin2017@foxmail.com>
2026-06-04 15:32:27 +0800

    手动引入element plus样式

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
+import 'element-plus/es/components/message/style/css'
+import 'element-plus/es/components/message-box/style/css'
+
 import './styles/reset.css'
 
 import App from './App.vue'
